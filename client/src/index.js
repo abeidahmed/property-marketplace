ecfaee218ab5e7aff1be469a349eb0913f980b72
abeidahmed <abeidahmed92@gmail.com>
2020-05-19 21:41:13 +0530

    Install tailwindcss

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "assets/stylesheet/font.css";
+import "assets/stylesheet/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
