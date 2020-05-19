97a270e68555151ba75322d2352fe9a132da03eb
abeidahmed <abeidahmed92@gmail.com>
2020-05-20 00:41:41 +0530

    Add link to homepage

--- client/src/views/home/index.js
+++ client/src/views/home/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1>hello from home</h1>
+      <Link to="/signup">Sign up</Link>
     </div>
   );
 };
