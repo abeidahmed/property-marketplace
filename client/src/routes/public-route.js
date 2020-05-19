f1e379b2f79a01483fff4e53c359d15a1949ccfa
abeidahmed <abeidahmed92@gmail.com>
2020-05-19 22:30:43 +0530

    Create signup form

--- client/src/routes/public-route.js
+++ client/src/routes/public-route.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Login from "views/login";
 import Home from "views/home";
+import Login from "views/login";
+import Signup from "views/signup";
 
 const PublicRoute = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </Switch>
   );
 };
