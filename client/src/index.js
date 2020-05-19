de8245e997f999ab11096199c10d87438fab15e7
abeidahmed <abeidahmed92@gmail.com>
2020-05-19 22:42:19 +0530

    Install graphql

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import App from "./App";
 import "assets/stylesheet/font.css";
 import "assets/stylesheet/main.css";
 
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
+  uri: "/graphql"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
