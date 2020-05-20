ce7f0d3437b862567a7d3c0f4535c23a93e36a2b
abeidahmed <abeidahmed92@gmail.com>
2020-05-21 00:10:38 +0530

    Disable button when signing up

--- client/src/views/signup/components/form.js
+++ client/src/views/signup/components/form.js
@@ -72,14 +72,17 @@ const Form = ({ signup, loading }) => {
         />
       </div>
       <div className="mt-6">
-        <button className="group relative block w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-7 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+        <button
+          disabled={loading}
+          className="group relative block w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-7 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+        >
           <span className="absolute left-0 inset-y-0 flex items-center pl-3">
             <Icon
               icon="lock"
               className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
             />
           </span>
-          Sign up
+          {loading ? "Signing up..." : "Sign up"}
         </button>
       </div>
     </form>
