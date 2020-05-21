96e645b2b06b0fec7d57205dc41695f85fba5ac0
abeidahmed <abeidahmed92@gmail.com>
2020-05-21 19:30:54 +0530

    Create sidebar initial

--- client/src/admin/routes/admin-route.js
+++ client/src/admin/routes/admin-route.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "admin/views/dashboard";
+import Sidebar from "admin/views/sidebar";
 import UserList from "admin/views/user/user-list";
 
 const AdminRouter = () => {
   return (
-    <Switch>
-      <Route path="/admin/dashboard" component={Dashboard} />
-      <Route path="/admin/users" component={UserList} />
-    </Switch>
+    <div className="flex">
+      <Sidebar />
+      <Switch>
+        <Route path="/admin/dashboard" component={Dashboard} />
+        <Route path="/admin/users" component={UserList} />
+      </Switch>
+    </div>
   );
 };
 
