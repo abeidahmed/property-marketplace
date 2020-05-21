401b04734ca418b24a408a771cad625cdf4c8989
abeidahmed <abeidahmed92@gmail.com>
2020-05-22 00:49:26 +0530

    Create a new date_time scalar type

---
+++ app/graphql/types/date_time.rb
@@ -0,0 +1,9 @@
+class Types::DateTime < Types::BaseScalar
+  def self.coerce_input(date_time, ctx)
+    Time.zone.parse(date_time)
+  end
+
+  def self.coerce_result(date_time, ctx)
+    date_time.strftime("#{date_time.day.ordinalize} %b, %Y")
+  end
+end
