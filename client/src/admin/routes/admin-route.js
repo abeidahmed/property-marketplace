1329f5001beab4ea9fa227524ff243626671cdec
abeidahmed <abeidahmed92@gmail.com>
2020-05-21 22:08:31 +0530

    Make sidebar responsive

--- client/src/admin/routes/admin-route.js
+++ client/src/admin/routes/admin-route.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "admin/views/dashboard";
+import Header from "admin/views/header";
 import Sidebar from "admin/views/sidebar";
 import UserList from "admin/views/user/user-list";
 
 const AdminRouter = () => {
+  const [sidebarActive, setSidebarActive] = useState(false);
+
   return (
     <div className="flex">
-      <Sidebar />
-      <Switch>
-        <Route path="/admin/dashboard" component={Dashboard} />
-        <Route path="/admin/users" component={UserList} />
-      </Switch>
+      <Sidebar sidebarActive={sidebarActive} setSidebarActive={setSidebarActive} />
+      <div className="flex flex-col w-full">
+        <Header setSidebarActive={setSidebarActive} />
+        <Switch>
+          <Route path="/admin/dashboard" component={Dashboard} />
+          <Route path="/admin/users" component={UserList} />
+        </Switch>
+      </div>
     </div>
   );
 };
--- client/src/admin/views/sidebar/index.js
+++ client/src/admin/views/sidebar/index.js
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 import Links from "./components/links";
 import LogoFullWhite from "assets/logo/logo-full-white.svg";
 
-const Sidebar = () => {
+const Sidebar = ({ setSidebarActive, sidebarActive }) => {
   return (
-    <aside className="transform lg:translate-x-0 fixed top-0 lg:sticky w-64 z-50 h-screen bg-gray-800 overflow-y-hidden flex flex-col border-r border-gray-200 bg-white flex-shrink-0 transition duration-150 ease-in-out">
-      <div className="h-16 flex flex-shrink-0 items-center justify-between px-3">
+    <aside
+      className={`${
+        sidebarActive ? "translate-x-0" : "-translate-x-full"
+      } transform lg:translate-x-0 fixed top-0 lg:sticky w-64 z-50 h-screen bg-gray-800 overflow-y-hidden flex flex-col border-r border-gray-200 bg-white flex-shrink-0 transition duration-150 ease-in-out`}
+    >
+      <div className="bg-gray-900 h-16 flex flex-shrink-0 items-center justify-between px-3">
         <Link to="/">
           <img src={LogoFullWhite} className="h-8 w-auto" alt="Property marketplace" />
         </Link>
-        <button className="lg:hidden p-1 -mr-2 rounded-md text-gray-700 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition duration-150 ease-in-out">
+        <button
+          onClick={() => setSidebarActive(false)}
+          className="lg:hidden p-1 -mr-2 rounded-md text-gray-200 focus:outline-none focus:text-white focus:bg-gray-800 transition duration-150 ease-in-out"
+        >
           <Icon icon="close" className="h-6 w-6" />
         </button>
       </div>
