862dba7fb5a281e43a4b3b12ca5ca72e81fb67d1
abeidahmed <abeidahmed92@gmail.com>
2020-05-22 00:49:44 +0530

    Format date time

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,13 +1,11 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
     field :full_name, String, null: false
     field :email, String, null: false
     field :password, String, null: false
-    field :created_at, String, null: false
-    field :updated_at, String, null: false
+    field :created_at, DateTime, null: false
+    field :updated_at, DateTime, null: false
 
     def full_name
       "#{object.first_name} #{object.last_name}"
